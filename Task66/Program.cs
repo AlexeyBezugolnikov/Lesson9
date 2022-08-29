660d3642a4aa21be4bade54e54bb4de94abab46f
Alexey Bezugolnikov <abezugolnikov@list.ru>
2022-08-29 22:26:18 +0300

    Lesson9

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿int f(int m, int n)
+ 
+
+
+{
+    
+    
+    if (n==m)
+         return n;
+    
+    return f(m,n-1) + n;
+}
+
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (m > n)
+     Console.WriteLine(f(n, m));
+ else
+     Console.WriteLine(f(m, n));
---
+++ Task68/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int f(int m, int n)
+
+
+
+{
+    if (m==0)
+    return (n+1);
+    if (m>0 && n == 0)
+    return f(m-1,1);
+    if (m>0 && n > 0)
+    return f(m-1,f(m,n-1));
+    return 0;
+}
+
+
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m<0)
+m=Math.Abs(m);
+if (n<0)
+n=Math.Abs(m);
+
+Console.WriteLine(f(m,n));
